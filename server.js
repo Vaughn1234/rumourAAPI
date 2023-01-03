21550e6006976d1156c9ee82a37df3a1ba9d919e
Vaughn1234 <elias.idel@web.de>
2023-01-03 10:20:19 +0100

    optimized for latency

--- server.js
+++ server.js
@@ -7,108 +7,48 @@ const { request } = require('express')
 const app = express()
 
 rumours = []
-sources = []
+sourcesDe = [];
+sourcesGb = [];
+sourcesEs = [];
+sourcesIt = [];
+sourcesFr = [];
 
 app.get('/', (req, res) => {
     res.json('Welcome to the Rumour Mill API.')
+    GetAllRumours();
 })
 
 app.get('/sources/DE1', (req, res) => {
-    axios.get('https://www.transfermarkt.de/bundesliga/geruechte/wettbewerb/L1')
-        .then((response) => {
-            sources = []
-            const html = response.data
-            const $ = cheerio.load(html)
-
-            const data = [...$(".items .odd, .items .even")].map(e => {
-                const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
-                currentClub = $(e).find(".zentriert a").attr('title')
-                otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
-                sources.push({
-                    playerName: row[0],
-                    currentClub: currentClub,
-                    interestedClub: row[1],
-                    url: otherRow[1]
-
-                })
-            });
-            res.json(sources)
-            })
+    res.json(sourcesDe);
         })
 
 app.get('/sources/GB1', (req, res) => {
-    axios.get('https://www.transfermarkt.de/premier-league/geruechte/wettbewerb/GB1')
-        .then((response) => {
-            sources = []
-            const html = response.data
-            const $ = cheerio.load(html)
-
-            const data = [...$(".items .odd, .items .even")].map(e => {
-                const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
-                currentClub = $(e).find(".zentriert a").attr('title')
-                otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
-                sources.push({
-                    playerName: row[0],
-                    currentClub: currentClub,
-                    interestedClub: row[1],
-                    url: otherRow[1]
-
-                })
-            });
-            res.json(sources)
-        })
+    res.json(sourcesGb);
 })
-
 app.get('/sources/es1', (req, res) => {
-    axios.get('https://www.transfermarkt.de/laliga/geruechte/wettbewerb/ES1')
-        .then((response) => {
-            sources = []
-            const html = response.data
-            const $ = cheerio.load(html)
-
-            const data = [...$(".items .odd, .items .even")].map(e => {
-                const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
-                currentClub = $(e).find(".zentriert a").attr('title')
-                otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
-                sources.push({
-                    playerName: row[0],
-                    currentClub: currentClub,
-                    interestedClub: row[1],
-                    url: otherRow[1]
-
-                })
-            });
-            res.json(sources)
-        })
+    res.json(sourcesEs);
 })
 
 app.get('/sources/it1', (req, res) => {
-    axios.get('https://www.transfermarkt.de/serie-a/geruechte/wettbewerb/IT1')
-        .then((response) => {
-            sources = []
-            const html = response.data
-            const $ = cheerio.load(html)
-
-            const data = [...$(".items .odd, .items .even")].map(e => {
-                const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
-                currentClub = $(e).find(".zentriert a").attr('title')
-                otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
-                sources.push({
-                    playerName: row[0],
-                    currentClub: currentClub,
-                    interestedClub: row[1],
-                    url: otherRow[1]
-
-                })
-            });
-            res.json(sources)
-        })
+    res.json(sourcesIt);
 })
 
 app.get('/sources/fr1', (req, res) => {
-    axios.get('https://www.transfermarkt.de/ligue-1/geruechte/wettbewerb/FR1')
+    res.json(sourcesFr);
+})
+
+function GetAllRumours() {
+    setInterval(GetEsRumours, 30000);
+    setInterval(GetDeRumours, 30000);
+    setInterval(GetGbRumours, 30000);
+    setInterval(GetItRumours, 30000);
+    setInterval(GetFrRumours, 30000);
+
+}
+
+function GetGbRumours() {
+    axios.get('https://www.transfermarkt.de/premier-league/geruechte/wettbewerb/GB1')
         .then((response) => {
-            sources = []
             const html = response.data
             const $ = cheerio.load(html)
 
@@ -116,7 +56,7 @@ app.get('/sources/fr1', (req, res) => {
                 const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
                 currentClub = $(e).find(".zentriert a").attr('title')
                 otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
-                sources.push({
+                sourcesGb.push({
                     playerName: row[0],
                     currentClub: currentClub,
                     interestedClub: row[1],
@@ -124,9 +64,94 @@ app.get('/sources/fr1', (req, res) => {
 
                 })
             });
-            res.json(sources)
         })
-})
+}
+
+function GetDeRumours() {
+    sourcesDe = [];
+    axios.get('https://www.transfermarkt.de/bundesliga/geruechte/wettbewerb/L1')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            const data = [...$(".items .odd, .items .even")].map(e => {
+                const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
+                currentClub = $(e).find(".zentriert a").attr('title')
+                otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
+                sourcesDe.push({
+                    playerName: row[0],
+                    currentClub: currentClub,
+                    interestedClub: row[1],
+                    url: otherRow[1]
+
+                })
+            });
+        })
+}
+
+function GetEsRumours() {
+    sourcesEs = [];
+    axios.get('https://www.transfermarkt.de/laliga/geruechte/wettbewerb/ES1')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            const data = [...$(".items .odd, .items .even")].map(e => {
+                const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
+                currentClub = $(e).find(".zentriert a").attr('title')
+                otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
+                sourcesEs.push({
+                    playerName: row[0],
+                    currentClub: currentClub,
+                    interestedClub: row[1],
+                    url: otherRow[1]
+                })
+            });
+        })
+}
+
+function GetItRumours() {
+    axios.get('https://www.transfermarkt.de/serie-a/geruechte/wettbewerb/IT1')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            const data = [...$(".items .odd, .items .even")].map(e => {
+                const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
+                currentClub = $(e).find(".zentriert a").attr('title')
+                otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
+                sourcesIt.push({
+                    playerName: row[0],
+                    currentClub: currentClub,
+                    interestedClub: row[1],
+                    url: otherRow[1]
+
+                })
+            });
+        })
+}
+
+function GetFrRumours() {
+    axios.get('https://www.transfermarkt.de/ligue-1/geruechte/wettbewerb/FR1')
+        .then((response) => {
+            const html = response.data
+            const $ = cheerio.load(html)
+
+            const data = [...$(".items .odd, .items .even")].map(e => {
+                const row = [...$(e).find(".hauptlink")].map(e => $(e).text().trim());
+                currentClub = $(e).find(".zentriert a").attr('title')
+                otherRow = [...$(e).find(".zentriert a")].map(e => $(e).attr('href').trim())
+                sourcesFr.push({
+                    playerName: row[0],
+                    currentClub: currentClub,
+                    interestedClub: row[1],
+                    url: otherRow[1]
+
+                })
+            });
+        })
+}
+
 
 app.get('/gb/rumours', (req, res) => {
     axios.get('https://www.transfermarkt.co.uk/rumour-mill/detail/forum/180')
